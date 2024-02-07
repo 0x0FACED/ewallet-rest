879d5dbb0cb8a979811ea80f3e1d0696e7be7279
MisterGnida <https.genius@gmail.com>
2024-02-07 15:18:11 +0300

    Updated Open method

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -1,1 +1,64 @@
 package store
+
+import (
+	"database/sql"
+	_ "github.com/lib/pq"
+)
+
+type Store struct {
+	Config        *Config
+	Db            *sql.DB
+	WalletDB      *WalletDB
+	TransactionDB *TransactionDB
+}
+
+func New(config *Config) *Store {
+	return &Store{
+		Config: config,
+	}
+}
+
+func (s *Store) Open() error {
+	//db, err := sql.Open("postgres", s.Config.DatabaseURL)
+	db, err := sql.Open("postgres", "postgres://postgres:lexacoolman@localhost/ewallet_db?sslmode=disable")
+	if err != nil {
+		return err
+	}
+	if err := db.Ping(); err != nil {
+		return err
+	}
+
+	s.WalletDB = NewWalletDB(s)
+	s.TransactionDB = NewTransactionDB(s)
+	s.Db = db
+
+	return nil
+}
+
+func (s *Store) Close() {
+	s.Db.Close()
+}
+
+func (s *Store) GetWalletDB() *WalletDB {
+	if s.WalletDB != nil {
+		return s.WalletDB
+	}
+
+	s.WalletDB = &WalletDB{
+		store: s,
+	}
+
+	return s.WalletDB
+}
+
+func (s *Store) GetTransactionDB() *TransactionDB {
+	if s.TransactionDB != nil {
+		return s.TransactionDB
+	}
+
+	s.TransactionDB = &TransactionDB{
+		store: s,
+	}
+
+	return s.TransactionDB
+}
