fe94f5c9aeb60002ac01c4e9fbea4b0b5b6974f3
MisterGnida <https.genius@gmail.com>
2024-02-07 15:33:39 +0300

    Added last migrations

---
+++ migrations/000004_new_tables.up.sql
@@ -0,0 +1,21 @@
+-- 4_up.sql
+
+-- Удаление существующих таблиц, если они существуют
+DROP TABLE IF EXISTS transactions;
+DROP TABLE IF EXISTS wallets;
+
+-- Создание новых таблиц с id type VARCHAR
+CREATE TABLE wallets
+(
+    id      VARCHAR PRIMARY KEY,
+    balance DECIMAL(10, 2) NOT NULL DEFAULT 100.0
+);
+
+CREATE TABLE transactions
+(
+    id           SERIAL PRIMARY KEY,
+    time         TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    sender_id    VARCHAR        NOT NULL,
+    recipient_id VARCHAR        NOT NULL,
+    amount       DECIMAL(10, 2) NOT NULL
+);
