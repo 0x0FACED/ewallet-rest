aca63f805548b00edc4c06e16dc7d41716cf0651
MisterGnida <https.genius@gmail.com>
2024-02-07 15:20:05 +0300

    Nothing interesting

--- pkg/server/config.go
+++ pkg/server/config.go
@@ -1,1 +1,15 @@
 package server
+
+import "ewallet/internal/app/store"
+
+type Config struct {
+	BindAddress string `toml:"bind_address"`
+	LogLevel    string `toml:"log_level"`
+	StoreCfg    *store.Config
+}
+
+func NewConfig() *Config {
+	return &Config{
+		StoreCfg: store.NewConfig(),
+	}
+}
