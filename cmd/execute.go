a32e81180d1ecd7e973b57f171b103de87b80f0a
MisterGnida <https.genius@gmail.com>
2024-02-07 15:19:40 +0300

    Added Execute method

---
+++ cmd/execute.go
@@ -0,0 +1,35 @@
+package cmd
+
+import (
+	"ewallet/pkg/server"
+	"flag"
+	"github.com/BurntSushi/toml"
+	"log"
+)
+
+var (
+	configPath string
+)
+
+func init() {
+	flag.StringVar(
+		&configPath,
+		"config-path",
+		"config/server.toml",
+		"path to config",
+	)
+}
+
+func Execute() {
+	flag.Parse()
+
+	config := server.NewConfig()
+	_, err := toml.DecodeFile(configPath, config)
+	if err != nil {
+		log.Fatal(err)
+	}
+	s := server.New(config)
+	if err := s.Start(); err != nil {
+		log.Fatal(err)
+	}
+}
