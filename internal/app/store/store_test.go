677c018f3d324f0e172fb45f2cec987af31b962c
MisterGnida <https.genius@gmail.com>
2024-02-08 17:40:52 +0300

    Created store tests

---
+++ internal/app/store/store_test.go
@@ -0,0 +1,27 @@
+package store
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func newTestStore(t *testing.T) *Store {
+	t.Helper()
+	return New()
+}
+
+func TestStore_Open(t *testing.T) {
+	s := newTestStore(t)
+	err := s.Open(dbUrl)
+	assert.NoError(t, err)
+}
+
+func TestStore_Close(t *testing.T) {
+	s := newTestStore(t)
+	defer func() {
+		if r := recover(); r != nil {
+			assert.NotNil(t, r)
+		}
+	}()
+	s.Close()
+}
