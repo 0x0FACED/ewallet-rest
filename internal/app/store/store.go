d14c61791f76f5206b7e841a617fbe24a700c69e
MisterGnida <https.genius@gmail.com>
2024-02-08 17:41:42 +0300

    Added slog.logger, removed config

--- internal/app/store/store.go
+++ internal/app/store/store.go
@@ -3,40 +3,54 @@ package store
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"log/slog"
 )
 
 type Store struct {
-	Config        *Config
+	Logger        *slog.Logger
 	Db            *sql.DB
 	WalletDB      *WalletDB
 	TransactionDB *TransactionDB
 }
 
-func New(config *Config) *Store {
+func New() *Store {
 	return &Store{
-		Config: config,
+		Logger: slog.Default(),
 	}
 }
 
-func (s *Store) Open() error {
-	//db, err := sql.Open("postgres", s.Config.DatabaseURL)
-	db, err := sql.Open("postgres", "postgres://postgres:lexacoolman@localhost/ewallet_db?sslmode=disable")
+func (s *Store) Open(databaseURL string) error {
+	db, err := sql.Open("postgres", databaseURL)
 	if err != nil {
+		s.Logger.Error("Failed to Open() db connection:", err)
 		return err
 	}
+	s.Logger.Info("SUCCESS: DB Open() successful")
 	if err := db.Ping(); err != nil {
+		s.Logger.Error("Failed to Ping() db:", err)
 		return err
 	}
+	s.Logger.Info("SUCCESS: DB Ping() successful")
 
 	s.WalletDB = NewWalletDB(s)
+	s.Logger.Info("NewWalletDB(s)")
 	s.TransactionDB = NewTransactionDB(s)
+	s.Logger.Info("NewTransactionDB(s)")
 	s.Db = db
 
+	s.Logger.Info("Method store.Open() returned nil")
 	return nil
 }
 
-func (s *Store) Close() {
-	s.Db.Close()
+func (s *Store) Close() error {
+	s.Logger.Info("Closing DB connection...")
+	err := s.Db.Close()
+	if err != nil {
+		s.Logger.Error("Failed to close DB connection")
+		return err
+	}
+	s.Logger.Info("SUCCESS: DB connection closed")
+	return nil
 }
 
 func (s *Store) GetWalletDB() *WalletDB {
